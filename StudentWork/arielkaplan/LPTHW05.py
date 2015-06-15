c0bef2b9fa3f0253fd6de7debbc3a283ad192aca
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-06-15 14:14:23 -0700

    Comment LPTHW06.py

--- StudentWork/arielkaplan/LPTHW05.py
+++ StudentWork/arielkaplan/LPTHW05.py
@@ -16,4 +16,5 @@ print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d, I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d, I get %d." % (
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
--- StudentWork/arielkaplan/LPTHW06.py
+++ StudentWork/arielkaplan/LPTHW06.py
@@ -1,5 +1,6 @@
-# EXERCISE 06
+# EXERCISE 06: Strings and Text
 
+# %d is decimal.
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
@@ -7,12 +8,19 @@ y = "Those who know %s and those who %s." % (binary, do_not)
 
 print x
 print y
+
+# %r is repr --> representation. Passes a string with quotes. 
+# Passes a bool as its representation False
 print "I said: %r." % x
+
+# %s is string. Passes a string without quotes.
 print "I also said: '%s'." % y
 
+# Can store a %r and fill it in later: 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny? %r"
 
+# This is where the %r is filled in
 print joke_evaluation % hilarious
 
 w = "This is the left side of..."
