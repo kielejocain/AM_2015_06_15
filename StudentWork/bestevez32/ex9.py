402b2251680e528628fab1090435f9581e1869f3
bestevez32 <bestevez32@gmail.com>
2015-06-15 16:10:36 -0700

    Learning Python ex9.py

---
+++ StudentWork/bestevez32/ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+month = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+I can type as much as I want.
+Even 4 lines or 5 or 6, I can keep going.
+"""
