1d7e8778094c022c51cb3f394d1e776ce5df0836
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-06-16 15:12:52 -0700

    Add 07_reverse.py

---
+++ StudentWork/arielkaplan/07_reverse.py
@@ -0,0 +1,37 @@
+# this script should take in a sentence or statement from the user,
+# then repeat that statement back with the words in reverse order.
+# Do not reverse the words themselves!
+
+statement = raw_input("Please enter a statment to reverse.\n>> ")
+
+words = []
+start = 0
+
+for i in range(len(statement)) :
+	if statement[i] == " " : 
+		end = i
+		words.append(statement[start:end])
+		start = i + 1
+		print words
+# add last word 
+words.append(statement[start:])
+print words
+
+reverse_statement = ""
+
+for word in words[::-1] :
+	reverse_statement += word
+	if word != words[0] :
+		reverse_statement += " "
+print reverse_statement
+
+
+# for char in statement[:] :
+# 	if char == " " : 
+# 		i = statement.index(char)
+# 		print i
+# 		words.append(statement[start:i])
+# 		# statement = statement[i:]
+# 		start = i
+# words.append(statement[start:])
+# print words
