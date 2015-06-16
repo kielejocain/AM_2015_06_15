7b0d66044b6ca63eac9c241a72296e17dc043e26
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-06-16 10:52:47 -0700

    Add LPTHW12.py

---
+++ StudentWork/arielkaplan/LPTHW/LPTHW12.py
@@ -0,0 +1,10 @@
+# EXERCISE 12: Prompting people
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you wight? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight
+	) # pep8 compliant to put paren here at same level of indent
+# ...or here.
