a89e4001b50ca432fe7b7b8280f435095b295887
JAKE RICHMOND <jakeorichmond@gmail.com>
2015-06-22 13:37:43 -0700

    Awesome Stuff

---
+++ StudentWork/jake_richmond/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Jake Richmond'
+my_age = 29
+my_height = 78 # inches
+my_weight = 275 # lbs
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
