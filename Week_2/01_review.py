2e2d0986845b4c2f41dcae90dae24ae97193e088
kielejocain <joecken@gmail.com>
2015-06-22 08:34:36 -0700

    Week 2 prep/cleanup

---
+++ Week_2/01_review.py
@@ -0,0 +1,35 @@
+## Review of Week 1 concepts
+
+# make a list of three movie titles.
+
+movies = []
+
+# using a loop and slicing (movies[start:end]), print the first five
+# characters and last five characters of each movie title.
+
+# you could also use a while loop if you like.
+
+for thing in list:
+    pass
+
+# make a dictionary where the keys are fruits and the values are quantities.
+# use at least three fruits.
+
+fruits = {}
+
+# define a function that takes two arguments and uses them to update
+# the quantity of a fruit.  Please rename the variables to something
+# relevant to their meaning.
+
+def count_update(arg1, arg2):
+    pass
+
+# Call the function to add 10 to the inventory of your favorite fruit,
+# add 5 to the inventory of your second favorite fruit, and subtract
+# 10 from the inventory of your least favorite fruit.
+
+
+
+# check to see that it worked!
+
+print fruits
