c9e0e5413b139cebd7d0233559776bbb3fce5a2e
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-06-26 14:27:54 -0700

    Add ex49 parser

---
+++ StudentWork/arielkaplan/LPTHW/ex48/ex48/parser.py
@@ -0,0 +1,80 @@
+class Sentence(object):
+	"""take ('noun', 'princess') tuples and convert them"""
+	def __init__(self, subject, verb, obj):
+		self.subject = subject[1]
+		self.verb = verb[1]
+		self.object = obj[1]
+
+	# From Kyle's code, equality functions to allow us to check 
+	# Sentence == Sentence directly
+	def __eq__(self, other):
+		return (self.subject == other.subject and 
+			   self.verb == other.verb and
+			   self.object == other.object)
+
+	def __ne__(self, other):
+		return not self.__eq__(other)
+
+class ParserError(Exception):
+	pass
+
+
+
+def peek(word_list):
+	if word_list:
+		word = word_list[0]
+		return word[0]
+	else:
+		return None
+
+def match(word_list, expecting):
+	if word_list:
+		word = word_list.pop(0)
+
+		if word[0] == expecting:
+			return word
+		else:
+			return None
+	else:
+		return None
+
+def skip(word_list, word_type):
+	while peek(word_list) == word_type:
+		match(word_list, word_type)
+
+def parse_verb(word_list):
+	skip(word_list, 'stop')
+
+	if peek(word_list) == 'verb':
+		return match(word_list, 'verb')
+	else: 
+		raise ParserError("Expected a verb next")
+
+def parse_object(word_list):
+	skip(word_list, 'stop')
+	next_word = peek(word_list)
+
+	if next_word == 'noun':
+		return match(word_list, 'noun')
+	elif next_word == 'direction':
+		return match(word_list, 'direction')
+	else:
+		raise ParserError("Expected a noun or direction next.")
+
+def parse_subject(word_list):
+	skip(word_list, 'stop')
+	next_word = peek(word_list)
+
+	if next_word == 'noun':
+		return match(word_list, 'noun')
+	elif next_word == 'verb':
+		return ('noun', 'player')
+	else:
+		raise ParserError("Expected a verb next.")
+
+def parse_sentence(word_list):
+	subj = parse_subject(word_list)
+	verb = parse_verb(word_list)
+	obj = parse_object(word_list)
+
+	return Sentence(subj, verb, obj)
