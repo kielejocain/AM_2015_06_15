d479aede0a3a1060f4248b056d97190314c22815
kevinelong <S!mpl312C0mplexity>
2015-06-30 16:32:31 -0700

    pocket change

---
+++ Week_3/pocket_change.py
@@ -0,0 +1,15 @@
+pocket_change = {
+    "pennies": 13,
+    "nickels": 3,
+    "dimes": 2,
+    "quarters": 4
+}
+
+
+def total(pocket_change):
+    amount = 0
+    # TODO calculate total dollar amount
+    return amount
+
+
+print(total(pocket_change))
