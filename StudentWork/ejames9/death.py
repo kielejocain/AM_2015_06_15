5694080120645f2b5dc7e87a680aeee73519bc0f
ejames9 <foster9@icloud.com>
2015-06-30 13:34:37 -0700

    add files

--- StudentWork/ejames9/death.py
+++
@@ -1,8 +0,0 @@
-
-# Fork class, which will be imported into my game.
-from sys import exit
-
-def death():
-    print "As you quickly near the falls, you frantically search for a way out, but to no avail! As your bed begins to fall, with your heart pounding wildly, you"
-    print "realize you are doomed! Suddenly, You awake in your bedroom, in your bed, soaked in your own sweat! No wiser than you were before. You Lose!"
-    exit(1)
