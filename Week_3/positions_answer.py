9ee24f463c365317d0a5d1ce76034746b94bc6a8
kevinelong <S!mpl312C0mplexity>
2015-07-01 14:42:26 -0700

    updated for clarity

--- Week_3/positions_answer.py
+++ Week_3/positions_answer.py
@@ -1,6 +1,6 @@
 from math import sqrt, hypot
 
-data = [[531, 408, 'A'], [225, 52, 'B'], [594, 242, 'C'], [351, 102, 'D'], [371, 15, 'E'], [569, 353, 'F'],
+location_list = [[531, 408, 'A'], [225, 52, 'B'], [594, 242, 'C'], [351, 102, 'D'], [371, 15, 'E'], [569, 353, 'F'],
         [342, 39, 'G'], [218, 304, 'H'], [428, 260, 'I'], [329, 158, 'J'], [585, 530, 'K'], [71, 114, 'L'],
         [587, 88, 'M'], [347, 180, 'N'], [180, 332, 'O'], [250, 522, 'P'], [88, 475, 'Q'], [260, 128, 'R'],
         [328, 505, 'S'], [381, 201, 'T'], [360, 192, 'U'], [414, 313, 'V'], [525, 293, 'W'], [240, 563, 'X'],
@@ -14,23 +14,28 @@ style = """
 """
 
 
-def distance(p1, p2):
-    # return (sqrt( (p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 ))
-    return (hypot(p2[0] - p1[0], p2[1] - p1[1]))
+def distance(point_from, point_to):
+    LATTITUDE = 0
+    LONGITUDE = 1
+    lateral_distance = point_from[LATTITUDE] - point_to[LATTITUDE]
+    longitudinal_distance = point_from[LONGITUDE] - point_to[LONGITUDE]
+    return sqrt(lateral_distance ** 2 + longitudinal_distance ** 2)
+    # return (hypot(p2[0] - p1[0], p2[1] - p1[1]))
+
 
 
 letter_dict = {}
 for d in data:
     letter = d[2]
-    letter_dict[ letter ] = d
+    letter_dict[letter] = d
 
 # for arbitrary value e.g. "J" find the nearest N items e.g. 5
 def get_top(data, letter, top_n):
     selected = letter_dict[letter]
     # Calculate distance and append as 4th element after lat, long, and letter
-    for v in data:
-        dist = distance(v, selected)
-        v.append(dist)
+    for location in data:
+        dist = distance(location, selected)
+        location.append(dist)
 
     s = sorted(data, key=lambda item: item[3])
     return s[0:top_n + 1]
@@ -62,6 +67,6 @@ f.close()
 
 # OUTPUT html for "J" and 5 would show R D N U and T highlighted.
 
-#CLUES - Ways of calculating distance between two points.
+# CLUES - Ways of calculating distance between two points.
 # dist = sqrt( (x2 - x1)**2 + (y2 - y1)**2 )
 # dist = math.h
