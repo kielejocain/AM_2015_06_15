a46228c9002465826da941e4c2beab52123b3384
JAKE RICHMOND <jakeorichmond@gmail.com>
2015-07-01 15:59:39 -0700

    week 3

--- StudentWork/jake_richmond/letter_frequency.py
+++ StudentWork/jake_richmond/letter_frequency.py
@@ -1,13 +1,33 @@
 # display the number of times each letter occurs
 sentence = "Now is the time for all good people to come to the aid of their planet."
 
-count = {}
-for letter in sentence:
-    if count.has_key(letter):
-        count[letter] += 1
-    else:
-        count[letter] = 1
 
-for key in count:
-    if count[key] > 1:
-        print key, count[key]
+def letter_count(sentence):
+    count = {}
+    for letter in sentence:
+        if count.has_key(letter):
+            count[letter] += 1
+        else:
+            count[letter] = 1
+
+    for key in count:
+        if count[key] >= 1:
+            print key, count[key]
+print letter_count(sentence)
+# What if we want to count capitals and lower as the same.
+
+sentence = sentence.lower()
+
+def lower_count(sentence):
+    count = {}
+    for letter in sentence:
+        if count.has_key(letter):
+            count[letter] += 1
+
+        else:
+            count[letter] = 1
+
+    for key in count:
+        if count[key] >= 1:
+            print key, count[key]
+print lower_count(sentence)
