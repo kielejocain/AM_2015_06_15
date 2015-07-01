ef718f5de03c000dca5363c0ffb11a85eba79b2c
Emily C <emilymargaretcain@gmail.com>
2015-07-01 13:34:39 -0700

    adding letter frequency program

--- Week_3/letter_freq.py
+++ Week_3/letter_freq.py
@@ -8,15 +8,17 @@ def letter_analysis(phrase):
     letter_dict = {}
     for letter in phrase.lower():
         if letter not in letter_dict.keys():
-            letter_dict[letter] = 1
+            letter_dict[letter] = 0
         letter_dict[letter] += 1
     return letter_dict
 
 def letter_display(ltr_dct):
     output = ""
     for c in lowercase[:]:
-        if c in ltr_dct.keys():
+        if c in ltr_dct.keys() and ltr_dct[c] > 1:
             output += c + " occurs " + str(ltr_dct[c]) + " times.\n"
+        elif c in ltr_dct.keys() and ltr_dct[c] == 1:
+            output += c + " occurs once.\n"
         else:
             output += c + " is not in the string.\n"
     return output
