6be1cd4ae7babbfca244c1f6b3686e063343dd53
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-07-02 09:54:19 -0700

    Update address_book

--- StudentWork/arielkaplan/address_book.py
+++ StudentWork/arielkaplan/address_book.py
@@ -22,7 +22,7 @@ class Person(object):
     def __str__(self):
         return ("Name: " + self.first_name + " " + self.last_name +
                 "\nPhone: " + self.phone +
-                "\nEmail: " + self.email +
+                "\nEmail: " + self.email
         )
 
 class AddressBook(object):
@@ -39,7 +39,10 @@ class AddressBook(object):
             # find last element of sorted id # and add one
             unique_id = self.ids[-1] + 1
         new_person.id = unique_id
-        self.people.append(new_person)
+        self.people[unique_id] = new_person
+        print ("You have added " + new_person.first_name + " " + new_person.last_name +
+               " to your address book.")
+        print new_person
 
     def read(self, parameter): # for now, assume parameter is id
         # for person in self.people:
@@ -63,6 +66,28 @@ class AddressBook(object):
     def delete(self, parameter): # for now, assume parameter is id
         id = parameter
         delete_person = self.people[id]
+        print delete_person
         confirm = raw_input("Are you sure you want to delete this person? y/n ")
-        if confirm.lower() == 'y'
+        if confirm.lower() == 'y':
+            del delete_person
+            print "Entry deleted."
+        else:
+            print "Delete aborted."
+
+    def find_id(self, parameter):
+        # check if
+        if parameter in self.people.keys():
+            return parameter
+
+
+test_first = "Jane"
+test_last = "Doe"
+test_phone = "555-123-4567"
+test_email = "jane@example.com"
+update_first = "John"
+
+my_addresses = AddressBook()
+my_addresses.create(test_first, test_last, test_phone, test_email)
+my_addresses.update(1, "first_name")
+
 
