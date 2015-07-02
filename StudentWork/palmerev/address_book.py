338a7de4c957ee27dc48d0d543ae2524f2edb1e9
Evan Palmer <evjpalmer@gmail.com>
2015-07-02 14:00:49 -0700

    add solution to address_book.py

--- StudentWork/palmerev/address_book.py
+++ StudentWork/palmerev/address_book.py
@@ -16,37 +16,49 @@ class Person(object):
         self.last_name = ""
         self.phone = ""
         self.email = ""
+        self.id = None
+
+    def __repr__(self):
+        return "'{first} {last}'".format(first=self.first_name, last=self.last_name)
 
 class AddressBook(object):
     id_number = 1
     def __init__(self):
-        #each entry inserted by add() will be a dict with two keys.
         self.entries = {}
 
     def add(self, person):
         #person must be a Person object
-        full_name = " ".join([person.first_name, person.last_name])
-        if full_name in self.entries:
-            print "There is already an entry with that name. Please choose a different name."
-            return
-        self.entries[full_name] = {"id": AddressBook.id_number, "contact info": person}
+        person.id = AddressBook.id_number
+        self.entries[person.id] = person
         AddressBook.id_number += 1
+        return person.id
+
     # "read"
-    def search(self, target_name):
-        #prints the contact info for target_name and returns True if found, else returns False
-        if target_name in self.entries:
-            entry = self.entries[target_name]
-            print "Found entry: "
-            print entry["contact info"].first_name, " ", entry["contact info"].last_name
-            print entry["contact info"].phone
-            print entry["contact info"].email
+    def search(self, id):
+        #prints the contact info for id and returns True if found, else returns False
+        if id in self.entries:
+            entry = self.entries[id]
+            print "Found entry:", entry
+            print "phone:", entry.phone
+            print "email:", entry.email
             return True
         else:
             print "Entry not found."
             return False
 
-    def update(self, target_name):
-        if self.search(target_name):
+    def update(self, id, first_name=None, last_name=None, phone=None, email=None):
+        person = self.entries[id]
+        if first_name is not None:
+            person.first_name = first_name
+        if last_name is not None:
+            person.last_name = last_name
+        if phone is not None:
+            person.phone = phone
+        if email is not None:
+            person.email = email
+
+    def update_from_raw_input(self, person):
+        if self.search(person):
             print "1) first name"
             print "2) last name"
             print "3) phone"
@@ -55,44 +67,14 @@ class AddressBook(object):
             while option not in [1, 2, 3, 4]:
                 option = raw_input("Which item would you like to change (1 - 4)? ")
             if option == 1:
-                old_person = self.entries[target_name]["contact info"]
                 new_first_name = raw_input("Enter a new first name: ")
-                new_full_name = " ".join(new_first_name, old_person.last_name)
-                while new_full_name in self.entries.keys():
-                    new_first_name = raw_input(
-                        "{name} already exists. Please choose a different first name: ".format(name=new_full_name)
-                    )
-                    new_full_name = " ".join([old_person.first_name, new_last_name])
-                new_person = Person()
-                new_person.first_name = new_first_name
-                new_person.last_name = old_person.last_name
-                new_person.phone = old_person.phone
-                new_person.email = old_person.email
-                self.add(new_person)
-                del self.entries[target_name]
+                self.entries[person.id].first_name = new_first_name
             elif option == 2:
-                old_person = self.entries[target_name]["contact info"]
                 new_last_name = raw_input("Enter a new last name: ")
-                new_full_name = " ".join([old_person.first_name, new_last_name])
-                while new_full_name in self.entries.keys():
-                    new_last_name = raw_input(
-                        "{name} already exists. Please choose a different last name: ".format(name=new_full_name)
-                    )
-                    new_full_name = " ".join([old_person.first_name, new_last_name])
-                new_person = Person()
-                new_person.first_name = old_person.first_name
-                new_person.last_name = new_last_name
-                new_person.phone = old_person.phone
-                new_person.email = old_person.email
-                self.add(new_person)
-                del self.entries[target_name]
+                self.entries[person.id].last_name = new_last_name
             elif option == 3:
                 new_phone = raw_input("Enter a new phone number: ")
-                person = self.entries[target_name]["contact info"]
                 person.phone = new_phone
-                print "Phone for {first} {last} is {phone}.".format(
-                    first=person.first_name, last=person.last_name, phone=person.phone
-                    )
             else:
                 new_email = raw_input("Enter a new email address: ")
                 person = self.entries[target_name]["contact info"]
@@ -100,20 +82,45 @@ class AddressBook(object):
                 print "Phone for {first} {last} is {email}.".format(
                     first=person.first_name, last=person.last_name, email=person.email
                     )
-    def delete(self, target_name):
-        if self.search(target_name):
+
+    def delete_with_confirmation(self, person):
+        if self.search(person.id):
             choice = None
             while choice.lower() not in ['y', 'n']:
-                choice = raw_input("Are you sure you want to delete {name}? (y/n) ".format(name=target_name))
+                full_name = person.first_name + " " + person.last_name
+                choice = raw_input("Are you sure you want to delete {name}? (y/n) ".format(full_name))
             if choice == 'y':
-                print "Deleted {name}.".format(name=target_name)
-                del self.entries[target_name]
+                print "Deleted {name}.".format(name=full_name)
+                del self.entries[full_name]
+
+    def delete(self, id):
+        try:
+            del self.entries[id]
+        except KeyError:
+            print "A person with id {x} doesn't exist.".format(x=id)
+
+    def get_all(self):
+        return str(self.entries)
 
 def test_AddressBook():
     book = AddressBook()
-    person = Person()
-    person.first_name = "Kevin"
-    person.last_name = "Long"
-    book.add(person)
+    person1 = Person()
+    person1.first_name = "Evan"
+    person1.last_name = "Palmer"
+    id1 = book.add(person1)
+    person2 = Person()
+    person2.first_name = "Kevin"
+    person2.last_name = "Long"
+    id2 = book.add(person2)
+    print(book.get_all())
+    book.update(id1, first_name="Python", last_name="is Awesome")
+    book.update(id2, last_name="is Cool")
+    print(book.get_all())
+    book.search(1)
+    book.search(2)
+    book.search(3)
+    book.delete(2)
+    book.delete(3)
+    print(book.get_all())
 
 test_AddressBook()
