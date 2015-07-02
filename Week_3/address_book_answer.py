763368ae4dc6c91b55ea33d7d371a3917cf418f3
kevinelong <S!mpl312C0mplexity>
2015-07-02 13:57:07 -0700

    updates

--- Week_3/address_book_answer.py
+++ Week_3/address_book_answer.py
@@ -2,14 +2,12 @@
 #       Include properties/attributes for
 #       a phone number, email address,
 #       and separate first and last name fields
-
 #  2. Create an address book class
 #       Initialize it with an empty dict of people
 #       Add methods to create, read, update, and delete.
 #       Each person added should be assigned a unique id.
 #       THe ID should be a simple number that gets incremented.
 #       e.g. 1, 2 , 3 etc
-
 
 class AddressBook():
     def __init__(self):
@@ -40,6 +38,13 @@ class AddressBook():
     def get_all(self):
         return self.people
 
+    def find_all_matches(self, property_name, property_value):
+        output = []
+        for i, p in self.people.items():
+            if p.__dict__[property_name] == property_value:
+                output.append(p)
+        return output
+
 
 class Person(object):
     def __init__(self):
@@ -54,23 +59,41 @@ class Person(object):
 # Using the classes the following test code should work
 def test_book():
     book = AddressBook()
+
     person = Person()
     person.first_name = "Kevin"
     person.last_name = "Long"
-
     id = book.add(person)
+
+    person = Person()
+    person.first_name = "Kay"
+    person.last_name = "Long"
+    id2 = book.add(person)
+
+    person = Person()
+    person.first_name = "John"
+    person.last_name = "Smith"
+    id3 = book.add(person)
 
     for i, item in book.get_all().items():
         print(item)
 
+    # reteive person by id
     p = book.get(id)
+
+    # change persons name
     p.first_name = "Nemo"
+
+    # update person to change name
     book.update(p)
 
     for i, item in book.get_all().items():
         print(item.id)
         print(item.first_name)
         print(item.last_name)
+
+    for item in book.find_all_matches("last_name", "Long"):
+        print(item.id, item.first_name, item.last_name)
 
 
 test_book()
