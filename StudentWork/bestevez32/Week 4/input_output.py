2faf2ae97d622c68cd3156d5786069456c6d6483
bestevez32 <bestevez32@gmail.com>
2015-07-06 16:01:37 -0700

    New Stuff

---
+++ StudentWork/bestevez32/Week 4/input_output.py
@@ -0,0 +1,23 @@
+#  TODO: ADVANCED
+#  This reads lines from the input file reverses their order and writes them into the output file.
+# How could we do this without the "all" variable,
+#  so we do not wast ram memory by loading the whole file.
+
+input_file = open("input_data.txt", "r")
+
+line = input_file.readline()
+all = []
+while line:
+    all.append(line)
+    line = input_file.readline()
+all.append(line)
+input_file.close()
+
+# reverse the order
+all.reverse()
+
+output_file = open("output_data.txt", "w")
+
+for line in all:
+    output_file.write(line + "\n")
+output_file.close()
