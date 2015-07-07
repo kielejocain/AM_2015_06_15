1a97962e10074e95e2bba46b79a7cfc5c6076051
kevinelong <S!mpl312C0mplexity>
2015-07-07 09:52:10 -0700

    WIP Work in progress

---
+++ Week_4/timer_challenge_answer_advanaced.py
@@ -0,0 +1,60 @@
+#   TODO:
+#   1.  Add increment and decrement methods for minutes and hours.
+#   2.  If Seconds is at 59 and you increment:
+#           A.  set seconds to zero
+#           B.  increment minute
+#   3.  Likewise if minutes is 59 set to zero and increment minutes
+#   4.  Do similar work for hours.
+
+class Timer(object):
+    MINUTES_IN_HOUR = 60
+    SECONDS_IN_MINUTE = 60
+
+    def __init__(self):
+        self.hours = 0
+        self.minutes = 0
+        self.seconds = 0
+
+    # add one hour
+    def increment_hours(self, amount):
+        self.hours += amount
+        if self.hours > 99:
+            self.hours = 0
+
+    # remove one hour
+    def decrement_hours(self, amount):
+        self.hours -= amount
+
+    # add one minute
+    def increment_minutes(self, amount):
+        self.minutes += amount
+        if self.minutes > 59:
+            self.increment_hours(1)
+            self.minutes = 0
+
+    # remove one minute
+    def decrement_minutes(self, amount):
+        self.minutes -= amount
+
+    # add one second
+    def increment(self, amount):
+        self.seconds += amount
+        minutes = int(self.seconds / self.SECONDS_IN_MINUTE)
+        self.increment_minutes(minutes)
+        self.seconds = self.seconds % self.SECONDS_IN_MINUTE
+
+    # remove one second
+    def decrement(self):
+        self.seconds -= amount
+
+
+def test_timer():
+    t = Timer()
+    t.minutes = 59
+    t.seconds = 59
+    t.increment(120)
+    print(t.hours, t.minutes, t.seconds)
+    # Should get one hour zero minutes and zero seconds
+
+
+test_timer()
