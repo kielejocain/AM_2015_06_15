76774d5e777b45b6615b2f6efba603de3dc7994f
ejames9 <foster9@icloud.com>
2015-07-15 13:07:56 -0700

    add files.

--- StudentWork/ejames9/projects/mysite/mysite/urls.py
+++ StudentWork/ejames9/projects/mysite/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from polls import views
 urlpatterns = [
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
 ]
