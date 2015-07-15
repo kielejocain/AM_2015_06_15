70d9fa5f49ec2e0a283a65f97f35f5efb036908c
kevinelong <S!mpl312C0mplexity>
2015-07-15 10:54:42 -0700

    django update

--- Week_5/django/mysite/polls/urls.py
+++ Week_5/django/mysite/polls/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # ex: /data/
+    url(r'^data/$', views.data, name='data'),  # ex: /data/
+    url(r'^api_vote/$', views.api_vote, name='api_vote'),
 ]
