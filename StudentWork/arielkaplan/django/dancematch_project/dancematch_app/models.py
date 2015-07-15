f4d0bf05bf2cb4a15f46e82c0ddd8ef9a5b21a6c
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-07-15 13:13:29 -0700

    Add dancematch_project setup files

---
+++ StudentWork/arielkaplan/django/dancematch_project/dancematch_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Dancer(models.Model):
+    pass
+
+class Dance(models.Model):
+    pass
+
+class Schedule(models.Model):
+    pass
+
+class Location(models.Model):
+    pass
