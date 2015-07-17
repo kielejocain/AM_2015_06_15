bd66803caf74d80bb2d44882814023c38ae28529
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-07-17 14:26:55 -0700

    Update dancematch: Models for dropdown choices in danceprefs

--- StudentWork/arielkaplan/django/dancematch_project/dancematch_app/views.py
+++ StudentWork/arielkaplan/django/dancematch_project/dancematch_app/views.py
@@ -7,6 +7,7 @@ from .models import Dancer, Dance, Schedule, Location, DancePrefs
 
 import json
 
+
 def index(request):
     all_dancers = Dancer.objects.all()
     template = loader.get_template('index.html')
@@ -15,10 +16,18 @@ def index(request):
     })
     return HttpResponse(template.render(context))
 
+
 def profile(request, dancer_id):
+    # Kevin's preferred method: less reliant on builtin functions; two steps
+    # filtered_dancer_list = Question.objects.filter(id=dancer_id)
+    # dancer = filtered_dancer_list[0]
     dancer = get_object_or_404(Dancer, pk=dancer_id)
-    dance_prefs = get_list_or_404(DancePrefs, dancer=dancer)
-    return render(request, 'profile.html', {'dancer': dancer, 'dance_prefs': dance_prefs})
+    # dance_prefs = get_list_or_404(DancePrefs, dancer=dancer)
+    dance_prefs = DancePrefs.objects.filter(dancer=dancer)
+    return render(request, 'profile.html', {'dancer': dancer,
+                                            'dance_prefs': dance_prefs,
+                                            })
+
 
 def dances(request):
     all_dances = Dance.objects.all()
@@ -28,7 +37,10 @@ def dances(request):
     })
     return HttpResponse(template.render(context))
 
+
 def edit(request, dancer_id):
     dancer = get_object_or_404(Dancer, pk=dancer_id)
     dance_prefs = get_list_or_404(DancePrefs, dancer=dancer)
-    return render(request, 'edit.html', {'dancer': dancer, 'dance_prefs': dance_prefs})
+    return render(request, 'edit.html', {'dancer': dancer,
+                                         'dance_prefs': dance_prefs,
+                                         })
